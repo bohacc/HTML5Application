23c7ee26f7243f621df902118496f94ad6c93152
bohac <bohac@Martin-8.notia.local>
2013-02-06 22:19:09 +0100

    Další work

--- public_html/Page2.js
+++ public_html/Page2.js
@@ -10,5 +10,6 @@ $(document).live('pageinit', function(event){
   regCtrl('atelefon2', 3, ['ds:web_adresar_zaz_pda_json', 'ds_par:&aparameters=code:'+getParQS('apartner'), 'field:telefon2']);
   regCtrl('amobil', 3, ['ds:web_adresar_zaz_pda_json', 'ds_par:&aparameters=code:'+getParQS('apartner'), 'field:mobil']);
   regCtrl('aemail', 3, ['ds:web_adresar_zaz_pda_json', 'ds_par:&aparameters=code:'+getParQS('apartner'), 'field:email']);
-  initDocs();cccc
+  initDocs();
+  
 });
