e5254bba80793fbdba33bee5aa72630f776a12c5
Martin Boháč <bohacc@seznam.cz>
2013-07-12 11:55:20 +0200

    Nový partner, nová osoba

---
+++ public_html/Page7.js
@@ -0,0 +1,24 @@
+/* Unit for init page 7 - Record CRM contacts
+ * Author: Martin Boháč
+ * Company: Notia
+ */
+
+// FUNCTION FOR CONTROLLERS
+
+
+// INICIALIZACE CONTROLLERU 
+
+$(document).bind('pageinit', function(event){
+    page = new Page(7);
+    initPage(page);
+    
+    regCtrl('#apartner', 3, ['ds:web_adresar_zaz2_pda_json', 'ds_par:&aparameters=code:'+getParam('aid'), 'field:partner', 'field_ref_val:partner']);
+    regCtrl('#atelefon', 1, ['ds:web_adresar_zaz2_pda_json', 'ds_par:&aparameters=code:'+getParam('aid'), 'field:telefon', 'save:hodnota;partneri_spojeni;cislo;cislo_tel']);
+    regCtrl('#aulice', 1, ['ds:web_adresar_zaz2_pda_json', 'ds_par:&aparameters=code:'+getParam('aid'), 'field:ulice', 'save:ulice;partneri_adresy;cislo;cislo_adresy']);
+    regCtrl('#amesto', 1, ['ds:web_adresar_zaz2_pda_json', 'ds_par:&aparameters=code:'+getParam('aid'), 'field:mesto', 'save:mesto;partneri_adresy;cislo;cislo_adresy']);
+    regCtrl('#apsc', 1, ['ds:web_adresar_zaz2_pda_json', 'ds_par:&aparameters=code:'+getParam('aid'), 'field:psc', 'save:psc;partneri_adresy;cislo;cislo_adresy']);
+    regCtrl('#astat', 1, ['ds:web_adresar_zaz2_pda_json', 'ds_par:&aparameters=code:'+getParam('aid'), 'field:stat', 'disabled:disabled']);
+    regCtrl('#aemail', 1, ['ds:web_adresar_zaz2_pda_json', 'ds_par:&aparameters=code:'+getParam('aid'), 'field:email', 'save:hodnota;partneri_spojeni;cislo;cislo_email']);
+
+    initDocs();
+});
